9233505d9967ba787b0a701dba5c03dce4079342
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-28 02:42:55 +0530

    Updated userRoutes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,9 +9,14 @@ import {
     refreshAccessToken,
     register,
     sendOTP,
-    updateProfile
+    updateProfile,
+    verifyConductor,
 } from "../controllers/user.controller.js";
-import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+    authorizedRoles,
+    isLoggedIn,
+    isVerified
+} from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
@@ -24,10 +29,15 @@ userRouter.route("/logout").get(isLoggedIn, logout);
 userRouter.route("/current-user").get(isLoggedIn, getCurrentUser);
 userRouter
     .route("/change-avatar")
-    .put(upload.single("avatar"), isLoggedIn, changeAvatar);
-userRouter.route("/change-password").put(isLoggedIn, changePassword);
-userRouter.route("/update-profile").put(isLoggedIn, updateProfile);
+    .put(upload.single("avatar"), isLoggedIn, isVerified, changeAvatar);
+userRouter
+    .route("/change-password")
+    .put(isLoggedIn, isVerified, changePassword);
+userRouter.route("/update-profile").put(isLoggedIn, isVerified, updateProfile);
 userRouter.route("/refresh-token").get(refreshAccessToken);
 userRouter.route("/delete-user").delete(isLoggedIn, deleteUser);
+userRouter
+    .route("/verify-conductor")
+    .post(isLoggedIn, isVerified, authorizedRoles("ADMIN"), verifyConductor);
 
 export default userRouter;
