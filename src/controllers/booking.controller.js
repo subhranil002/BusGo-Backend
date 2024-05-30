2f780e0d134d69a1121adc8a8423b19d21fafe19
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-30 23:23:03 +0530

    Added getAllTickets, getTicket

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -6,6 +6,8 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { sendEmail } from "../utils/sendMail.js";
 import { formatInTimeZone } from "date-fns-tz";
+import { startOfDay, endOfDay } from "date-fns";
+import { ro } from "date-fns/locale";
 
 export const createBooking = asyncHandler(async (req, res, next) => {
     try {
@@ -26,11 +28,11 @@ export const createBooking = asyncHandler(async (req, res, next) => {
             throw new ApiError("All fields are required", 400);
         }
 
-        // // Validate route ID
-        // const isRouteIDValid = await BusRouteMap.findOne({ routeID });
-        // if (!isRouteIDValid) {
-        //     throw new ApiError("Invalid route ID", 400);
-        // }
+        // Validate route ID
+        const isRouteIDValid = await BusRouteMap.findOne({ routeID });
+        if (!isRouteIDValid) {
+            throw new ApiError("Invalid route ID", 400);
+        }
 
         // Validate conductor
         const isConductor = await User.findOne({
@@ -62,24 +64,6 @@ export const createBooking = asyncHandler(async (req, res, next) => {
         if (!booking) {
             throw new ApiError("Error while creating booking", 404);
         }
-
-        // Add booking to passenger's booking history and conductor's selling history
-        await User.updateOne(
-            { _id: passengerID },
-            {
-                $push: {
-                    bookingHistory: booking._id
-                }
-            }
-        );
-        await User.updateOne(
-            { _id: isConductor._id },
-            {
-                $push: {
-                    sellingHistory: booking._id
-                }
-            }
-        );
 
         // Send email to passenger
         const subject = "Booking Confirmation: Your BusGo Ticket Details";
@@ -122,6 +106,114 @@ export const createBooking = asyncHandler(async (req, res, next) => {
         return next(
             new ApiError(
                 `booking.controller :: createBooking :: ${error}`,
+                error.statusCode
+            )
+        );
+    }
+});
+
+export const getAllTickets = asyncHandler(async (req, res, next) => {
+    try {
+        // Fetch tickets
+        const tickets = await Booking.aggregate([
+            {
+                $match: {
+                    passengerID: req.user._id
+                }
+            },
+            {
+                $match: {
+                    bookingTime: {
+                        $gte: formatInTimeZone(
+                            startOfDay(new Date()),
+                            "Asia/Kolkata",
+                            "yyyy-MM-dd HH:mm:ss"
+                        ),
+                        $lte: formatInTimeZone(
+                            endOfDay(new Date()),
+                            "Asia/Kolkata",
+                            "yyyy-MM-dd HH:mm:ss"
+                        )
+                    }
+                }
+            },
+            {
+                $project: {
+                    conductorID: 0,
+                    passengerID: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    __v: 0
+                }
+            },
+            {
+                $sort: {
+                    bookingTime: 1
+                }
+            }
+        ]);
+
+        // Check if tickets exist
+        if (!tickets.length) {
+            return res
+                .status(200)
+                .json(new ApiResponse("No tickets booked today", []));
+        }
+
+        // Send response
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    "Today's bookings fetched successfully",
+                    tickets
+                )
+            );
+    } catch (error) {
+        return next(
+            new ApiError(
+                `booking.controller :: getAllTickets :: ${error}`,
+                error.statusCode
+            )
+        );
+    }
+});
+
+export const getTicket = asyncHandler(async (req, res, next) => {
+    try {
+        // Get ticket ID from params
+        const ticketID = req.params.ticketID;
+
+        // Validate ticket ID
+        if (!ticketID) {
+            throw new ApiError("Ticket ID is required", 400);
+        }
+
+        // Find ticket
+        const ticket = await Booking.findById(ticketID);
+
+        // Check if ticket exists
+        if (!ticket) {
+            throw new ApiError("Ticket not found", 404);
+        } else if (
+            req.user.role !== "ADMIN" &&
+            ticket.passengerID.toString() !== req.user._id.toString() &&
+            ticket.conductorID.toString() !== req.user._id.toString()
+        ) {
+            throw new ApiError(
+                `${req.user.role} is not authorized to view ticket`,
+                401
+            );
+        }
+
+        // Send response
+        return res
+            .status(200)
+            .json(new ApiResponse("Ticket fetched successfully", ticket));
+    } catch (error) {
+        return next(
+            new ApiError(
+                `booking.controller :: getTicket :: ${error}`,
                 error.statusCode
             )
         );
