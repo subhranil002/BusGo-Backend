8ca186301b7d597eff1f3771e8f4bd5cd08d347b
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-30 23:23:59 +0530

    Updated bookingRoutes

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,8 +1,40 @@
 import { Router } from "express";
 const bookingRouter = Router();
-import { createBooking } from "../controllers/booking.controller.js";
-import { isLoggedIn, isVerified } from "../middlewares/auth.middleware.js";
+import {
+    createBooking,
+    getAllTickets,
+    getSellingHistory,
+    getTicket,
+    verifyTicket
+} from "../controllers/booking.controller.js";
+import {
+    authorizedRoles,
+    isLoggedIn,
+    isVerified
+} from "../middlewares/auth.middleware.js";
 
 bookingRouter.route("/create").post(isLoggedIn, isVerified, createBooking);
+bookingRouter
+    .route("/get-all-tickets")
+    .get(isLoggedIn, isVerified, getAllTickets);
+bookingRouter
+    .route("/get-ticket/:ticketID")
+    .get(isLoggedIn, isVerified, getTicket);
+bookingRouter
+    .route("/verify-ticket/:ticketID")
+    .put(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("CONDUCTOR", "ADMIN"),
+        verifyTicket
+    );
+bookingRouter
+    .route("/selling-history")
+    .get(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("CONDUCTOR", "ADMIN"),
+        getSellingHistory
+    );
 
 export default bookingRouter;
